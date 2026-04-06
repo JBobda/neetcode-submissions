c00380418f3ccc05b74505d78c96b8abccab85e5
Jan Bobda <J.BobdaT@gmail.com>
2026-04-06 17:35:11 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        backTracking(nums, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backTracking(int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if(current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(current.contains(nums[i])) continue;
+
+            current.add(nums[i]);
+            backTracking(nums, current, result);
+            current.removeLast();
+        }
+    }
+}
